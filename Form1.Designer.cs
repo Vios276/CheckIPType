64782899225373deb9264f6ace040d46f407be9d
Vios276 <vios276@naeilsg.com>
2022-07-28 14:21:34 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace CheckIPType
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblLocalIP = new System.Windows.Forms.Label();
+            this.lblSubNet = new System.Windows.Forms.Label();
+            this.lblGateway = new System.Windows.Forms.Label();
+            this.lblPublicIP = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "내 아이피 확인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "외부IP :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "SubNet :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Gateway :";
+            // 
+            // lblLocalIP
+            // 
+            this.lblLocalIP.BackColor = System.Drawing.Color.White;
+            this.lblLocalIP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLocalIP.Location = new System.Drawing.Point(95, 78);
+            this.lblLocalIP.Name = "lblLocalIP";
+            this.lblLocalIP.Size = new System.Drawing.Size(186, 28);
+            this.lblLocalIP.TabIndex = 8;
+            this.lblLocalIP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSubNet
+            // 
+            this.lblSubNet.BackColor = System.Drawing.Color.White;
+            this.lblSubNet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSubNet.Location = new System.Drawing.Point(95, 116);
+            this.lblSubNet.Name = "lblSubNet";
+            this.lblSubNet.Size = new System.Drawing.Size(186, 28);
+            this.lblSubNet.TabIndex = 9;
+            this.lblSubNet.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGateway
+            // 
+            this.lblGateway.BackColor = System.Drawing.Color.White;
+            this.lblGateway.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGateway.Location = new System.Drawing.Point(95, 156);
+            this.lblGateway.Name = "lblGateway";
+            this.lblGateway.Size = new System.Drawing.Size(186, 28);
+            this.lblGateway.TabIndex = 10;
+            this.lblGateway.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPublicIP
+            // 
+            this.lblPublicIP.BackColor = System.Drawing.Color.White;
+            this.lblPublicIP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPublicIP.Location = new System.Drawing.Point(95, 195);
+            this.lblPublicIP.Name = "lblPublicIP";
+            this.lblPublicIP.Size = new System.Drawing.Size(186, 28);
+            this.lblPublicIP.TabIndex = 11;
+            this.lblPublicIP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(301, 186);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(174, 55);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Lime;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Location = new System.Drawing.Point(352, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 24);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "KT 공유기";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.FlatAppearance.CheckedBackColor = System.Drawing.Color.Lime;
+            this.radioButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton2.Location = new System.Drawing.Point(352, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 24);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "SK 공유기";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Enabled = false;
+            this.radioButton3.FlatAppearance.CheckedBackColor = System.Drawing.Color.Lime;
+            this.radioButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton3.Location = new System.Drawing.Point(352, 72);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(73, 24);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "LG 공유기";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Enabled = false;
+            this.radioButton4.FlatAppearance.CheckedBackColor = System.Drawing.Color.Lime;
+            this.radioButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton4.Location = new System.Drawing.Point(341, 102);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(98, 24);
+            this.radioButton4.TabIndex = 16;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "IPTIME 공유기";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Enabled = false;
+            this.radioButton5.FlatAppearance.CheckedBackColor = System.Drawing.Color.Lime;
+            this.radioButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton5.Location = new System.Drawing.Point(347, 132);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(81, 24);
+            this.radioButton5.TabIndex = 17;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "사설 공유기";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(345, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 12);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "공유기 구성도";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 252);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lblPublicIP);
+            this.Controls.Add(this.lblGateway);
+            this.Controls.Add(this.lblSubNet);
+            this.Controls.Add(this.lblLocalIP);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "공유기 확인";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblLocalIP;
+        private System.Windows.Forms.Label lblSubNet;
+        private System.Windows.Forms.Label lblGateway;
+        private System.Windows.Forms.Label lblPublicIP;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Net.NetworkInformation;
+
+namespace CheckIPType
+{
+    public partial class Form1 : Form
+    {
+        private const string KT = "172.30.1.254";
+        private const string LG = "192.168.219.1";
+        private const string SK = "192.168.35.1";
+        private const string IPTIME = "192.168.0.1";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+        }
+
+        private async Task RefreshPage()
+        {
+            var localIp = GetLocalIP();
+            var publicIp = await CheckPublicIP();
+            lblPublicIP.Text = publicIp;
+
+            if (localIp == publicIp)
+            {
+                //직결상황
+            }
+            else
+            {
+                //공유기 확인
+                //kt 172.30.1.254
+                //LG 192.168.219.1
+                //SK 192.168.35.1
+                //iptime 192.168.0.1
+
+                string dns = "168.126.63.1";
+
+                switch (lblGateway.Text)
+                {
+                    case KT:
+                        radioButton1.Checked = true;
+                        dns = "168.126.63.1";
+                        break;
+
+                    case SK:
+                        radioButton2.Checked = true;
+                        dns = "210.220.163.82";
+                        break;
+
+                    case LG:
+                        radioButton3.Checked = true;
+                        dns = "164.124.101.2";
+                        break;
+
+                    case IPTIME:
+                        radioButton4.Checked = true;
+                        break;
+
+                    default:
+                        radioButton5.Checked = true;
+                        break;
+                }
+
+                var ipList = GetTraceRoute(dns).ToList();
+                richTextBox1.AppendText("현재 컴퓨터\n");
+                var counter = 0;
+                for (int i = 0; i < 2; i++)
+                {
+                    switch (ipList[i].ToString())
+                    {
+                        case KT:
+                            richTextBox1.AppendText("KT 공유기\n");
+                            break;
+
+                        case SK:
+                            richTextBox1.AppendText("SK 공유기\n");
+                            break;
+
+                        case LG:
+                            richTextBox1.AppendText("LG 공유기\n");
+                            break;
+
+                        case IPTIME:
+                            richTextBox1.AppendText("LG 공유기\n");
+                            break;
+
+                        default:
+                            if (counter++ != 0)
+                            {
+                                i = 3;
+                                break;
+                            }
+                            richTextBox1.AppendText("사설 공유기 or 외부 라우터\n");
+                            break;
+                    }
+                }
+            }
+        }
+
+        private string GetLocalIP()
+        {
+            ManagementObjectSearcher ipAddress =
+                new ManagementObjectSearcher("Select * from Win32_NetworkAdapterConfiguration Where IPEnabled = 'True'");
+
+            foreach (ManagementObject obj in ipAddress.Get())
+            {
+                if (obj["IPAddress"] == null) continue;
+                if (!(obj["IPAddress"] is Array)) continue;
+
+                //WMI 인스턴스의 ["IPAddress"]에 해당하는 값을 string 배열로 Casting
+                //0 번쨰가 ip에 해당한다.
+                lblLocalIP.Text = ((string[])obj["IPAddress"])[0];
+
+                // Subnet Mask 를 가지고 올때, 만약 string[] 과 같은 형식 캐스팅이 불안하다면,
+                // 이와 같이 Linq의 OfType로 형식을 확인후 진행이 가능하다.
+                var subnetMask = ((IEnumerable)obj["IPSubnet"]).OfType<string>()
+                    .Select(f => f.ToString()).ToList();
+
+                lblSubNet.Text = subnetMask[0];
+
+                lblGateway.Text = ((string[])obj["DefaultIPGateway"])[0];
+            }
+
+            return lblLocalIP.Text;
+        }
+
+        private async Task<string> CheckPublicIP()
+        {
+            HttpClient client = new HttpClient();
+            client.Timeout = new TimeSpan(0, 0, 10);
+            var response = await client.GetAsync("https://api.ip.pe.kr/");
+
+            var publicIp = await response.Content.ReadAsStringAsync();
+
+            return publicIp;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            lblLocalIP.Text = string.Empty;
+            lblSubNet.Text = string.Empty;
+            lblGateway.Text = string.Empty;
+            lblPublicIP.Text = string.Empty;
+
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            radioButton5.Checked = false;
+
+            richTextBox1.Text = string.Empty;
+
+            await RefreshPage();
+        }
+
+        public static IEnumerable<IPAddress> GetTraceRoute(string hostname)
+        {
+            // following are similar to the defaults in the "traceroute" unix command.
+            const int timeout = 10000;
+            const int maxTTL = 30;
+            const int bufferSize = 32;
+
+            byte[] buffer = new byte[bufferSize];
+            new Random().NextBytes(buffer);
+
+            using (var pinger = new Ping())
+            {
+                for (int ttl = 1; ttl <= maxTTL; ttl++)
+                {
+                    PingOptions options = new PingOptions(ttl, true);
+                    PingReply reply = pinger.Send(hostname, timeout, buffer, options);
+
+                    // we've found a route at this ttl
+                    if (reply.Status == IPStatus.Success || reply.Status == IPStatus.TtlExpired)
+                        yield return reply.Address;
+
+                    // if we reach a status other than expired or timed out, we're done searching or there has been an error
+                    if (reply.Status != IPStatus.TtlExpired && reply.Status != IPStatus.TimedOut)
+                        break;
+                }
+            }
+        }
+    }
+}
